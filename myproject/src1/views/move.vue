eac4f60740e0be53abbd8e7e1abb447532162362
Xuanxiaojing <546292379@qq.com>
2017-10-26 20:58:23 +0800

    1026-mono-细分目录的整合版

---
+++ myproject/src1/views/move.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="cummunity">
+        这是move界面		
+    </div>
+</template>
+
+<script>
+    import FooterNav from '@/components/footer'
+    import SlideShow from '@/components/slide-show'
+
+    export default {
+        components:{
+            FooterNav,
+            SlideShow
+        }
+    }
+</script>
