8288bcd1a3c357206932574b5b1e2380f7463dc2
Xuanxiaojing <546292379@qq.com>
2017-11-09 22:10:21 +0800

    一开始有默认的播放列表，滑屏优化，改进搜索功能，解决电影图片报错

---
+++ 移动端/移动端MONO-吴雪爽-王晨莉-轩晓婧/MONO移动端作品-吴雪爽-王晨莉-轩晓婧-1109/project/src/api/urls.js
@@ -0,0 +1,21 @@
+
+let baseUrl = 'http://192.168.0.104:3200/api'
+let urls1 = {
+    listData: '/users',
+    books:"/books",
+    hometitle:"/hometitle",
+    find:"/find",
+    comunity:'/comunity'
+}
+for(var attr in urls1){
+    urls1[attr] = baseUrl + urls1[attr]
+}
+let urls2 = {
+    movie:"https://api.douban.com/v2/movie/in_theaters",
+}
+
+
+export default {
+    urls1,
+    urls2
+};
